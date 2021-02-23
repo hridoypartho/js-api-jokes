3841a8f3e13482050ee685f0ab560dd4eac4e662
hridoypartho <hridoyhalderpartho@gmail.com>
2021-02-23 22:48:38 +0600

    async data add

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 const jokes = document.querySelector('.project__content_joke');
 const jokesBtn = document.querySelector('.project__jokeBtn');
 
-const generateJokes = () => {
+// Non  Async data no:1
+
+/*const generateJokes = () => {
 
     const setHeader = {
         headers : {
@@ -16,7 +18,26 @@ const generateJokes = () => {
         }).catch((error) => {
              console.log(error);
         });
-}
+}*/
+
+
+// Async data no:2
+
+const generateJokes = async () => {
+    try{
+        const setHeader = {
+            headers : {
+                Accept: 'application/json'
+            }
+        }
+        const res = await fetch('https://icanhazdadjoke.com', setHeader);
+        const data = await res.json();
+        jokes.innerHTML = data.joke;
+    }catch(error){
+        console.log(error);
+    }
+};
+
 
 jokesBtn.addEventListener('click', generateJokes);
 generateJokes();
