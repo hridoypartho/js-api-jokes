6606237785f6a283fa5aa788d3d0304335b66b62
hridoypartho <hridoyhalderpartho@gmail.com>
2021-02-23 22:22:41 +0600

    complete joke projecrt

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,22 @@
 const jokes = document.querySelector('.project__content_joke');
 const jokesBtn = document.querySelector('.project__jokeBtn');
+
+const generateJokes = () => {
+
+    const setHeader = {
+        headers : {
+            Accept: 'application/json'
+        }
+    };
+
+    fetch('https://icanhazdadjoke.com', setHeader)
+        .then((res) => res.json())
+        .then( (data) => {
+            jokes.innerHTML = data.joke;
+        }).catch((error) => {
+             console.log(error);
+        });
+}
+
+jokesBtn.addEventListener('click', generateJokes);
+generateJokes();
